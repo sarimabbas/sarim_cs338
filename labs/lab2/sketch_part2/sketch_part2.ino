79e31689fe63230c5bd7a643e77f1cd8aa5727e1
Sarim Abbas <sarim.stays@gmail.com>
2019-02-16 19:50:03 -0500

    part 1 done

---
+++ labs/lab2/sketch_part2/sketch_part2.ino
@@ -0,0 +1,11 @@
+int P2 = 2;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(P2, INPUT);
+
+}
+
+// I took the advice on Piazza and adapted the code from the Arduino website
+void loop() {
+}
