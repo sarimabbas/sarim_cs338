d00bbe4c21a3261599c51cb74f69c3316e013f56
Sarim Abbas <sarim.stays@gmail.com>
2019-04-26 20:09:07 -0400

    controller works

--- labs/project/project_part5/controller.py
+++ labs/project/project_part5/controller.py
@@ -1,80 +1,84 @@
 import pygame
 import math
+import serial
+import json
 
+# init joystick stuff
 pygame.init()
 pygame.joystick.init()
 clock = pygame.time.Clock()
 done = False
 
-# -------- Main Program Loop -----------
 joystick_count = pygame.joystick.get_count()
-print("Number of joysticks: {}".format(joystick_count))
+if joystick_count < 1:
+    print("No controller found")
+    exit()
 
-for i in range(joystick_count):
-    joystick = pygame.joystick.Joystick(i)
-    joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
 
-print("Joystick {}".format(i) )
-
-# Get the name from the OS for the controller/joystick
+# Get the name and axes from the OS for the controller/joystick
 name = joystick.get_name()
-print("Joystick name: {}".format(name) )
-
-# Usually axis run in pairs, up/down for one, and left/right for
-# the other.
+print("Joystick name: {}".format(name))
 axes = joystick.get_numaxes()
-print("Number of axes: {}".format(axes) )
+print("Number of axes: {}".format(axes))
 
-while done==False:
-    # EVENT PROCESSING STEP
-    for event in pygame.event.get(): # User did something
+# gripper flag
+gripper = False
+neutral = False
 
-        # print(event)
+# attempt to open a Serial port to Arduino
+ser = serial.Serial(port='/dev/cu.usbmodem14101', baudrate=9600)
+print(ser.name)
 
+# helper function
+def approximateMovement(floating):
+    if math.isclose(floating, 1.0, rel_tol=1e-1):
+        return 1
+    elif math.isclose(floating, -1.0, rel_tol=1e-1):
+        return -1
+    else:
+        return 0
+
+while done == False:
+
+    # stick processing
+    # axes:
+    #   0 / left-thumb LR
+    #   1 / left-thumb UP
+    #   2 / right-thumb LR
+    #   3 / right-thumb UP
+    llr = approximateMovement(joystick.get_axis(0))
+    lup = approximateMovement(joystick.get_axis(1))
+    rlr = approximateMovement(joystick.get_axis(2))
+    rup = approximateMovement(joystick.get_axis(3))
+
+    # event-driven button processing
+    for event in pygame.event.get():
         # quit condition
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-        
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
+        if event.type == pygame.QUIT:
+            done = True     
+        # X button: 0
+        if event.type == pygame.JOYBUTTONDOWN and event.button == 0:
+            if event.button == 0:
+                gripper = not gripper
 
-        
-        if event.type == pygame.JOYAXISMOTION:  
+    # construct json object
+    controls = {
+        "llr": llr,
+        "lup" : lup,
+        "rlr" : rlr,
+        "rup": rup,
+        "gr": gripper,
+    }
 
-            # left thumb, left-right
-            if event.axis == 0:
-                if math.isclose(event.value, 1.0, rel_tol=1e-2):
-                        print("left stick: moved right")
-                if math.isclose(event.value, -1.0, rel_tol=1e-2):
-                        print("left stick: moved left")
-                if math.isclose(event.value, 0.0, rel_tol=1e-2):
-                        print("left stick: neutral")
-            # right thumb, left-right
-            if event.axis == 2:
-                if math.isclose(event.value, 1.0, rel_tol=1e-2):
-                        print("right stick: moved right")
-                if math.isclose(event.value, -1.0, rel_tol=1e-2):
-                        print("right stick: moved left")
-                if math.isclose(event.value, 0.0, rel_tol=1e-2):
-                        print("right stick: neutral")
-            # right thumb, up-down
-            if event.axis == 3:
-                if math.isclose(event.value, 1.0, rel_tol=1e-2):
-                        print("right stick: moved down")
-                if math.isclose(event.value, -1.0, rel_tol=1e-2):
-                        print("right stick: moved up")
-                if math.isclose(event.value, 0.0, rel_tol=1e-2):
-                        print("right stick: neutral")
+    controls_json = json.dumps(controls)
+    print(controls_json)
+    ser.write(bytes(controls_json, encoding='utf-8'))
 
-            # print("Joystick axis ", event.joy, event.axis, event.value)
-    
-    # Limit to 20 frames per second
-    clock.tick(20)
-    
+    # Limit frames per second
+    clock.tick(15)
+
 # Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit ()
+pygame.quit()
+
